4f38ae4f8fc431a539ccc9313ce3a28122676e36
Uttran Ishtalingam <uishtalingam@pros.com>
2024-06-02 11:08:01 -0500

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  output: "export",
+  distDir: "dist",
+};
 
 export default nextConfig;
